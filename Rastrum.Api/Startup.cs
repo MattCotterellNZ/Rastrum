01c0b63237f89e5fa1079efa884866375bea36a0
Matt Cotterell <matt@cotterell.net.nz>
2017-05-16 15:22:25 +1200

    Add API Versioning support

--- Rastrum.Api/Startup.cs
+++ Rastrum.Api/Startup.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -28,6 +29,7 @@ namespace Rastrum.Api
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
+            services.AddApiVersioning(o => o.ApiVersionReader = new HeaderApiVersionReader("api-version"));
             services.AddMvc();
         }
 
